189c0ed3cc9bce41fe753c97c63066ef43390c85
wzk <2434485443@qq.com>
2021-08-02 09:56:45 +0800

    create

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  title: "JankinWang",
+  description: "编程永远不止步",
+
+  themeConfig: {
+    logo: "/assets/img/logo.gif",
+    nav: [
+      { text: "Css", link: "/css/css" },
+      { text: "Gitee", link: "https://gitee.com/" },
+    ],
+
+    sidebar: {
+      "/css/": [
+        "css",
+        "动画",
+        "代码片段",
+        "变形 transform",
+        "媒体查询",
+
+        {
+          title: "布局", // 必要的
+          sidebarDepth: 2, // 可选的, 默认值是 1
+          children: ["栅格布局", "弹性盒子"],
+        },
+
+        {
+          title: "sassAndscss",
+          path: "/sassAndscss/目录结构设计",
+          children: ["/sassAndscss/目录结构设计"],
+        },
+      ],
+
+      "/sassAndscss/": ["目录结构设计"],
+    },
+  },
+};
---
+++ docs/css/css.md
@@ -0,0 +1,50 @@
+# CSS
+
+## 字体属性
+
+```css
+font-family
+font-size
+font-weight
+font-style
+
+font-variant
+font-stretch
+font-size-adjust
+
+font: italic 900 small-caps 15px;
+```
+
+
+
+
+
+## 文本属性
+
+```css
+text-indent /*缩进*/
+text-align
+line-height
+vertical-align
+
+word-spacing 字间隔（中文），单词间隔（英文）
+letter-spacing 字间隔
+
+文本阴影
+text-shadow
+
+文本转换：全大写、全小写、首字母大写
+text-transform
+
+文本修饰： 下划线、上划线、删除线、文本闪烁
+text-decoration
+
+空白字符的处理方式（如：pre不让浏览器合并多个空格，就像<pre>的效果）
+white-space
+
+文本方向 (从左往右读 或 相反)
+direction
+unicode-bidi
+
+```
+
